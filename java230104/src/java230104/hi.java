96cd754336b28f1d8ecaee18f3a1af94927b7ad6
박경호 <ossetian@naver.com>
2023-01-04 18:17:23 +0900

    hihihihihi

---
+++ java230104/src/java230104/hi.java
@@ -0,0 +1,12 @@
+package java230104;
+
+public class hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello!");
+		System.out.println("hello!");
+		System.out.println("hello!");
+	}
+
+}
